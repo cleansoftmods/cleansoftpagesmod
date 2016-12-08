79e493612bc28ea8a0927ee982b732fc24f6c669
Tedozi Manson <duyphan.developer@gmail.com>
2016-12-09 00:51:10 +0700

    Refactor

--- routes/web.php
+++ routes/web.php
@@ -6,42 +6,45 @@
  *
  */
 
-$router->group(['middleware' => 'web'], function (Router $router) {
+$adminRoute = config('webed.admin_route');
 
-    $adminRoute = config('webed.admin_route');
+$moduleRoute = 'pages';
 
-    $moduleRoute = 'pages';
+/*
+ * Admin route
+ * */
+$router->group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
+    $router->get('/', 'PageController@getIndex')
+        ->name('admin::pages.index.get')
+        ->middleware('has-permission:view-pages');
 
-    /*
-     * Admin route
-     * */
-    $router->group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
-        $router->get('/', 'PageController@getIndex')
-            ->name('admin::pages.index.get')
-            ->middleware('has-permission:view-pages');
+    $router->post('/', 'PageController@postListing')
+        ->name('admin::pages.index.post')
+        ->middleware('has-permission:view-pages');
 
-        $router->post('/', 'PageController@postListing')
-            ->name('admin::pages.index.post')
-            ->middleware('has-permission:view-pages');
+    $router->post('update-status/{id}/{status}', 'PageController@postUpdateStatus')
+        ->name('admin::pages.update-status.post')
+        ->middleware('has-permission:edit-pages');
 
-        $router->post('update-status/{id}/{status}', 'PageController@postUpdateStatus')
-            ->name('admin::pages.update-status.post')
-            ->middleware('has-permission:edit-pages');
+    $router->get('create', 'PageController@getCreate')
+        ->name('admin::pages.create.get')
+        ->middleware('has-permission:create-pages');
 
-        $router->get('create', 'PageController@getCreate')
-            ->name('admin::pages.create.get')
-            ->middleware('has-permission:create-pages');
+    $router->get('edit/{id}', 'PageController@getEdit')
+        ->name('admin::pages.edit.get')
+        ->middleware('has-permission:edit-pages');
 
-        $router->get('edit/{id}', 'PageController@getEdit')
-            ->name('admin::pages.edit.get')
-            ->middleware('has-permission:edit-pages');
+    $router->post('edit/{id}', 'PageController@postEdit')
+        ->name('admin::pages.edit.post')
+        ->middleware('has-permission:edit-pages');
 
-        $router->post('edit/{id}', 'PageController@postEdit')
-            ->name('admin::pages.edit.post')
-            ->middleware('has-permission:edit-pages');
-
-        $router->delete('/{id}', 'PageController@deleteDelete')
-            ->name('admin::pages.delete.delete')
-            ->middleware('has-permission:delete-pages');
-    });
+    $router->delete('/{id}', 'PageController@deleteDelete')
+        ->name('admin::pages.delete.delete')
+        ->middleware('has-permission:delete-pages');
 });
+
+/**
+ * Front site
+ */
+$router->get('/{slug?}', 'Front\ResolvePagesController@handle')
+    ->name('front.web.resolve-pages.get');
---
+++ src/Http/Controllers/Front/ResolvePagesController.php
@@ -0,0 +1,88 @@
+<?php namespace WebEd\Base\Pages\Http\Controllers\Front;
+
+use WebEd\Base\Core\Http\Controllers\BaseFrontController;
+use WebEd\Base\Pages\Models\Contracts\PageModelContract;
+use WebEd\Base\Pages\Models\EloquentPage;
+use WebEd\Base\Pages\Repositories\Contracts\PageContract;
+use WebEd\Base\Pages\Repositories\PageRepository;
+
+class ResolvePagesController extends BaseFrontController
+{
+    /**
+     * @var PageContract|PageRepository
+     */
+    protected $repository;
+
+    /**
+     * SlugWithoutSuffixController constructor.
+     * @param PageRepository $repository
+     */
+    public function __construct(PageContract $repository)
+    {
+        parent::__construct();
+
+        $this->themeController = \ThemesManagement::getThemeController('Page');
+
+        $this->repository = $repository;
+    }
+
+    public function handle($slug = null)
+    {
+        if(!$slug) {
+            $page = $this->repository
+                ->where('id', '=', do_filter('front.default-homepage.get', get_settings('default_homepage')))
+                ->where('status', '=', 'activated')
+                ->first();
+        } else {
+            $page = $this->repository
+                ->where('slug', '=', $slug)
+                ->where('status', '=', 'activated')
+                ->first();
+        }
+
+        if(!$page) {
+            if ($slug === null) {
+                echo '<h2>You need to setup your default homepage. Create a page then go through to Admin Dashboard -> Configuration -> Settings</h2>';
+                die();
+            } else {
+                abort(404);
+            }
+        }
+
+        $page = do_filter('front.web.resolve-pages.get', $page);
+
+        /**
+         * Update view count
+         */
+        increase_view_count($page, $page->id);
+
+        \AdminBar::registerLink('Edit this page', route('admin::pages.edit.get', ['id' => $page->id]));
+
+        $this->getMenu('page', $page->id);
+
+        $this->setPageTitle($page->title);
+
+        $this->dis['object'] = $page;
+
+        if($this->themeController) {
+            return $this->themeController->handle($page, $this->dis);
+        }
+
+        $happyMethod = '_template_' . studly_case($page->page_template);
+
+        if(method_exists($this, $happyMethod)) {
+            return $this->$happyMethod($page);
+        }
+
+        return $this->defaultTemplate($page);
+    }
+
+    /**
+     * @param EloquentPage $page
+     * @return mixed
+     */
+    protected function defaultTemplate(PageModelContract $page)
+    {
+        return $this->view('front.page-templates.default');
+    }
+}
