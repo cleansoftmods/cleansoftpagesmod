754d7082a7d856456c3c0e3bc6fdcbe2d7e8bf1f
Tedozi Manson <duyphan.developer@gmail.com>
2017-01-28 23:45:48 +0700

    Refactor

--- src/Http/Requests/CreatePageRequest.php
+++ src/Http/Requests/CreatePageRequest.php
@@ -7,11 +7,11 @@ class CreatePageRequest extends Request
     public $rules = [
         'page_template' => 'string|max:255|nullable',
         'title' => 'string|max:255|required',
-        'slug' => 'string|max:255|required',
-        'description' => 'string|max:1000',
-        'content' => 'string',
-        'thumbnail' => 'string|max:255',
-        'keywords' => 'string|max:255',
+        'slug' => 'string|max:255|nullable',
+        'description' => 'string|max:1000|nullable',
+        'content' => 'string|nullable',
+        'thumbnail' => 'string|max:255|nullable',
+        'keywords' => 'string|max:255|nullable',
         'status' => 'string|required|in:activated,disabled',
         'order' => 'integer|min:0',
     ];
